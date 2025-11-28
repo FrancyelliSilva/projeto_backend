b0010d428b90601b47e838c21043ec3cfc8c4ade
Francyelli Silva <127361802+FrancyelliSilva@users.noreply.github.com>
2025-11-27 21:33:25 -0300

    Remove database connection string from code
    Removed the connection string for the database.

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -4,7 +4,6 @@ async function conectaBancoDeDados(){
 	try{
 	    console.log('Conexão com o banco de dados iniciou')
 	
-	    await  mongoose.connect('mongodb+srv://francyellipereirasilva_db_user:60rZmMjbIGYtc17a@clustermulheres.o5mbhax.mongodb.net/?appName=ClusterMulheres')
 
 	    console.log('Conexão com o banco de dados feita com sucesso!')
     } catch(erro) {
