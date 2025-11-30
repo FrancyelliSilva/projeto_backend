91310a07a3d5a306c39f783837b2380ea287a366
Francyelli <francyelli.pereirasilva@gmail.com>
2025-11-30 19:52:10 -0300

    retornando dados server.js

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 const express = require("express")
-const app = express()
 
-// CORREÇÃO 1: Permite que o Render escolha a porta. 
-// Se não houver porta definida (no seu PC), usa a 3333.
-const porta = process.env.PORT || 3333
-
-// CORREÇÃO 2: Cria uma rota raiz para não dar erro "Cannot GET"
-app.get('/', (req, res) => {
-    res.send('Olá! O servidor está rodando corretamente no Render! 🚀')
-})
+const app = express ()
+const porta = 3333
 
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta ", porta)
